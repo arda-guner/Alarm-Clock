24be08fe2b61694939dcc062264f36c0c00c500a
arda-guner <169084062+arda-guner@users.noreply.github.com>
2026-05-03 12:08:46 +0300

    Update buzzer_pwm.v

--- src/buzzer_pwm.v
+++ src/buzzer_pwm.v
@@ -1,32 +1,37 @@
 `timescale 1ns/1ps
 
 module buzzer_pwm #(
-    parameter CLK_FREQ = 100_000_000,
-    parameter BUZZ_FREQ = 2000
+    parameter CLK_FREQ  = 100_000_000, // Nexys A7 clock frequency
+    parameter BUZZ_FREQ = 2000         // Buzzer tone frequency: 2 kHz
 )(
     input  wire clk,
     input  wire rst,
     input  wire enable,
-    output reg  pwm
+    output reg  buzzer
 );
 
+    //------------------------------------------------------------
+    // Half-period counter
+    // For 100 MHz and 2 kHz:
+    // HALF_PERIOD = 100_000_000 / (2 * 2000) = 25_000 cycles
+    //------------------------------------------------------------
     localparam integer HALF_PERIOD = CLK_FREQ / (2 * BUZZ_FREQ);
 
     reg [$clog2(HALF_PERIOD)-1:0] cnt;
 
     always @(posedge clk) begin
         if (rst) begin
-            cnt <= 0;
-            pwm <= 1'b0;
+            cnt    <= 0;
+            buzzer <= 1'b0;
         end
         else if (!enable) begin
-            cnt <= 0;
-            pwm <= 1'b0;
+            cnt    <= 0;
+            buzzer <= 1'b0;
         end
         else begin
             if (cnt == HALF_PERIOD - 1) begin
-                cnt <= 0;
-                pwm <= ~pwm;
+                cnt    <= 0;
+                buzzer <= ~buzzer;
             end
             else begin
                 cnt <= cnt + 1'b1;
@@ -34,4 +39,4 @@ module buzzer_pwm #(
         end
     end
 
-endmodule 
+endmodule
